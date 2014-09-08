1818af59e8d32c862e5695c5559b43081f7ef4a8
jianhel <luojianhe1992@163.com>
2014-09-07 23:11:40 -0400

    1

--- hw0-jianhel/src/main/java/DependencyExample.java
+++ hw0-jianhel/src/main/java/DependencyExample.java
@@ -1,7 +1,7 @@
 import java.io.StringReader;
 
 import edu.stanford.nlp.ling.Word;
-import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.objectbank.TokenizerFactory;
 import edu.stanford.nlp.process.PTBTokenizer.PTBTokenizerFactory;
 import edu.stanford.nlp.process.Tokenizer;
 
